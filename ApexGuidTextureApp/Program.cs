d38b1afa1fc2ea1f5660af919640398358d128b5
Dyvine57 <dulana57@gmail.com>
2022-05-05 18:40:55 -0400

    Add project files.

---
+++ ApexGuidTextureApp/Program.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApexGuidTextureApp {
+    internal class Program {
+        static void Main(string[] args) {
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string databaseName = "\\output.json";
+            string folderName = "\\_images";
+
+            bool missingFiles = false;
+            if (!File.Exists(path + databaseName)) {
+                Console.WriteLine("Missing json file " + path + databaseName);
+                missingFiles = true;
+            }
+            if (!Directory.Exists(path + folderName)) {
+                Console.WriteLine("Missing directory " + path + folderName);
+                missingFiles = true;
+            }
+            if (missingFiles) return;
+
+            string json = File.ReadAllText(path + databaseName);
+            var values = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+
+            var files = Directory.EnumerateFiles(path + folderName).ToList();
+            int count = files.Count();
+
+            for (int i = 0; i < count; i++) {
+                string fileName = Path.GetFileNameWithoutExtension(files[i]);
+                if (!fileName.Contains("0x")) {
+                    Console.WriteLine("Skipping " + fileName);
+                }
+                else {
+                    string fileGUID = fileName.Substring(0, 2) + fileName.Substring(2).ToUpper();
+                    string outValue;
+                    bool hasValue = values.TryGetValue(fileGUID, out outValue);
+                    if (hasValue) {
+                        if (outValue.Contains("\"t")) {
+                            outValue = outValue.Substring(outValue.LastIndexOf("]") + 1);
+                            outValue = outValue.Substring(0, outValue.Length - 1);
+                        }
+                        outValue = Path.GetFileNameWithoutExtension(outValue);
+                        File.Move(files[i], files[i].Replace(fileGUID.ToLower(), outValue));
+                        Console.WriteLine($"Renaming {fileGUID.ToLower()} to {outValue}");
+                    }
+                    else {
+                        Console.WriteLine("Missing Key " + fileGUID);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ApexGuidTexturePreprocess/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Threading;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ApexGuidTexturePreprocess {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public string inputDir;
+
+        public MainWindow() {
+            InitializeComponent();
+            txtb_inputpath.Text = inputDir;
+        }
+
+
+        private void ProcessRSON() {
+            Mouse.OverrideCursor = Cursors.Wait;
+            string outputFile = inputDir + "\\Output\\output.json";
+            Directory.CreateDirectory(Path.GetDirectoryName(outputFile));
+
+            Dictionary<string, string> guidDict = new Dictionary<string, string>();
+
+            var files = Directory.EnumerateFiles(inputDir, "*.rson").ToList();
+            int count = files.Count();
+            
+
+            for (int i = 0; i < count; i++) {
+                TextStatus.Dispatcher.Invoke(() => TextStatus.Text = "Processing " + Path.GetFileName(files[i]), DispatcherPriority.Background);
+                TextNumber.Dispatcher.Invoke(() => TextNumber.Text = $"{i}/{count}", DispatcherPriority.Background);
+
+                using (StreamReader openfile = File.OpenText(files[i])) {
+                    string fileString = openfile.ReadToEnd();
+                    fileString = fileString.Substring(fileString.IndexOf("memoryInfo"));
+
+
+                    List<string> listGuid = new List<string>();
+                    List<string> listPath = new List<string>();
+
+                    foreach (Match match in Regex.Matches(fileString, "(?<=guid: )(.*?)(?=\\r)|(?<=pak: )(.*?)(?=\\r)")) {
+                        if (match.Groups[1].Value != "") listGuid.Add(match.Groups[1].Value);
+                        if (match.Groups[2].Value != "") listPath.Add(match.Groups[2].Value);
+
+                    }
+
+                    for (int listIndex = 0; listIndex < listGuid.Count; listIndex++) {
+                        if (guidDict.ContainsKey(listGuid[listIndex].ToString())) {
+
+                        }
+                        else {
+                            guidDict.Add(listGuid[listIndex].ToString(), listPath[listIndex].ToString());
+                        }
+                    }
+                }
+            }
+
+            TextNumber.Text = "";
+            TextStatus.Text = "Writing Output File";
+
+            File.WriteAllText(outputFile, JsonConvert.SerializeObject(guidDict, Formatting.Indented));
+
+            TextStatus.Text = "";
+            Mouse.OverrideCursor = null;
+
+            MessageBox.Show("Finished", "Finished Generating", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+
+        private void btn_inputselect_Click(object sender, RoutedEventArgs e) {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.Title = "Select Input Folder";
+            dialog.InitialDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok) {
+                inputDir = dialog.FileName;
+                txtb_inputpath.Text = inputDir;
+            }
+        }
+
+        private void btn_inputopen_Click(object sender, RoutedEventArgs e) {
+            Process.Start(inputDir);
+        }
+
+        private void btn_run_Click(object sender, RoutedEventArgs e) {
+            if (inputDir == null) {
+                string title = "Empty";
+                string message = "Empty";
+                MessageBox.Show(message, title, MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            ProcessRSON();
+        }
+    }
+}
