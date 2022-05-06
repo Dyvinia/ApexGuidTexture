9818a645c28ca7ce12a536638e84e8591f7b17f2
Dyvine57 <dulana57@gmail.com>
2022-05-05 21:24:32 -0400

    Apparently 'handle' unhandled exceptions

--- ApexGuidTexturePreprocess/App.xaml.cs
+++ ApexGuidTexturePreprocess/App.xaml.cs
@@ -5,11 +5,16 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace ApexGuidTexturePreprocess {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e) {
+            MessageBox.Show("Error" + Environment.NewLine + e.Exception.Message, "Error");
+            e.Handled = true;
+        }
     }
 }
